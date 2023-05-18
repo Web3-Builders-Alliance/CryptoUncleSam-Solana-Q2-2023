662c5485b5bedf625d1c7f28086036d0bce3a3d2
CryptoUncleSam <tiosam@DESKTOP-L0BRGT6>
2023-05-18 21:02:14 +0200

    anchor

--- cluster1/wba-vault/programs/wba-vault/src/lib.rs
+++ cluster1/wba-vault/programs/wba-vault/src/lib.rs
@@ -1,15 +1,42 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("ANWZywNbnqNqrXxz2Vv79QLtDqW4BQL4iUyaz26whbdX");
 
 #[program]
 pub mod wba_vault {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.vault_state.score = 0;
+        ctx.accounts.vault_state.bump = *ctx.bumps.get("vault_auth").unwrap();
+        ctx.accounts.vault_state.auth_bump = *ctx.bumps.get("vault").unwrap();
+        ctx.accounts.vault_state.owner = *ctx.accounts.owner.key;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize <'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    #[account(init, payer=owner, space=Vault::LEN)]
+    pub vault_state: Account<'info, Vault>,
+    /// CHECK
+    #[account(seeds = [b"auth", vault_state.key().as_ref()], bump)]
+    pub vault_auth : UncheckedAccount<'info>,
+    #[account(seeds = [b"vault", vault_auth.key().as_ref()], bump)]
+    pub vault: SystemAccount <'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Vault {
+    pub owner: Pubkey,
+    pub bump: u8,
+    pub auth_bump: u8,
+    pub score: u8,
+}
+
+impl Vault {
+    const LEN: usize =8 + 32 + 1 + 1 + 1;
+}
--- cluster1/wba-vault/tests/wba-vault.ts
+++ cluster1/wba-vault/tests/wba-vault.ts
@@ -1,16 +1,75 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { WbaVault } from "../target/types/wba_vault";
+import { WbaVault, IDL } from "../target/types/wba_vault";
 
-describe("wba-vault", () => {
+
+
+
+describe("wba_vault", () => {
   // Configure the client to use the local cluster.
+    // Configure the client to use the local cluster.
+    const keypair = anchor.web3.Keypair.generate();
+  
+  const connection = new anchor.web3.Connection("http://localhost:8899");
   anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = new anchor.AnchorProvider(connection, new anchor.Wallet(keypair), { commitment: "finalized" } );
+  // Program address
+  const programAddres = new anchor.web3.PublicKey("ANWZywNbnqNqrXxz2Vv79QLtDqW4BQL4iUyaz26whbdX");
 
-  const program = anchor.workspace.WbaVault as Program<WbaVault>;
+  // Create program
+  const program = new Program<WbaVault>(IDL, programAddres, provider);
+  
+
+  
+
+  const vaultState =anchor.web3.Keypair.generate();
+
+// Create PDA VAULT AUTH
+
+const vault_auth_seeds = [Buffer.from("auth"), vaultState.publicKey.toBuffer()];
+const vault_auth = anchor.web3.PublicKey.findProgramAddressSync(vault_auth_seeds, program.programId)[0];
+
+// Create Vault system Program
+const vault_seeds = [Buffer.from("vault"), vault_auth.toBuffer()];
+const vault = anchor.web3.PublicKey.findProgramAddressSync(vault_seeds, program.programId)[0];
+
+
+
+it("Airdrop", async () => {
+  // 1. Airdrop 100 SOL to payer
+  const signature = await provider.connection.requestAirdrop(keypair.publicKey, 0.2 * 1_000_000_000);
+  const latestBlockhash = await connection.getLatestBlockhash();
+  await provider.connection.confirmTransaction(
+  {
+      signature,
+      ...latestBlockhash,
+  },
+"finalized"
+  );  
+})
+
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    try {
+    const txhash = await program.methods
+    .initialize()
+    .accounts({
+        owner: keypair.publicKey,
+        vaultState: vaultState.publicKey,
+        vaultAuth: vault_auth,
+        vault:vault,
+        systemProgram: anchor.web3.SystemProgram.programId,
+    
+    })
+    .signers([
+        keypair,
+        vaultState
+
+    ]).rpc();   
+    console.log(`Success! Check out your TX here: 
+    https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
+    }
+    catch(e){console.error(e)}
   });
 });
