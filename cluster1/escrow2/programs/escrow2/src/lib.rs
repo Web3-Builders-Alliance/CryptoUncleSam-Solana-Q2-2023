e76e1bfb20d57eefe2dea4b4a419fff3adf3a2b7
CryptoUncleSam <tiosam@DESKTOP-L0BRGT6>
2023-06-05 16:41:17 +0200

    devnet test and deploy

--- cluster1/escrow2/programs/escrow2/src/lib.rs
+++ cluster1/escrow2/programs/escrow2/src/lib.rs
@@ -54,7 +54,7 @@ pub mod escrow {
             Transfer {
                 from: ctx.accounts.vault.to_account_info(),
                 to: ctx.accounts.maker_ata.to_account_info(),
-                authority: ctx.accounts.maker.to_account_info(),
+                authority: ctx.accounts.vault_auth.to_account_info(),
             },
             signer_seeds,
         );
@@ -125,7 +125,6 @@ pub struct Cancel<'info> {
         mut,
         seeds=[b"state".as_ref(), escrow_seed.to_le_bytes().as_ref(), maker.key.as_ref()], 
         bump,
-        space = EscrowState::LEN,
         close = maker,
         has_one = maker,
         has_one = maker_ata,
@@ -135,7 +134,35 @@ pub struct Cancel<'info> {
           
 #[derive(Accounts)]
 pub struct Exchange<'info> {
-    
+    #[account(mut)]
+    pub taker: Signer<'info>,
+    #[account(mut, associated_token::mint = mint, associated_token::authority = taker)]
+    pub taker_ata: Account<'info, TokenAccount>,
+    #[account(seeds=[b"authority".as_ref()], bump = escrow_state.vault_auth_bump)]
+    pub vault_auth: UncheckedAccount<'info>,
+    pub mint: Account<'info, Mint>,
+    #[account(
+        seeds=[b"vault", escrow_state.escrow_seed.to_le_bytes().as_ref(), escrow_state.key.as_ref()], 
+        bump,
+        token::mint = mint,
+        token::authority = vault_auth,
+    )]
+    pub vault: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        seeds=[b"state".as_ref(), escrow_seed.to_le_bytes().as_ref(), maker.key.as_ref()], 
+        bump,
+        close = maker,
+        has_one = maker,
+        has_one = maker_ata,
+    )]
+    pub escrow_state: Account<'info, EscrowState>,
+    #[account(mut)]
+    pub taker_receive_ata: Account<'info, TokenAccount>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub taker_receive_ata: Account<'info, TokenAccount>,
 }
 
 #[account]
@@ -155,76 +182,3 @@ impl EscrowState {
     //pubkey is 32 bytes
     pub const LEN: usize = 8 + 32*3 + 3 + 8*2;
 }
-
-impl<'info> Initialize<'info> {
-    fn into_transfer_to_pda_context(
-        &self,
-    ) -> CpiContext<'_, '_, '_, 'info, TransferChecked<'info>> {
-        let cpi_accounts = TransferChecked {
-            from: self.initializer_deposit_token_account.to_account_info(),
-            mint: self.mint.to_account_info(),
-            to: self.vault.to_account_info(),
-            authority: self.initializer.to_account_info(),
-        };
-        CpiContext::new(self.token_program.to_account_info(), cpi_accounts)
-    }
-}
-
-impl<'info> Cancel<'info> {
-    fn into_transfer_to_initializer_context(
-        &self,
-    ) -> CpiContext<'_, '_, '_, 'info, TransferChecked<'info>> {
-        let cpi_accounts = TransferChecked {
-            from: self.vault.to_account_info(),
-            mint: self.mint.to_account_info(),
-            to: self.initializer_deposit_token_account.to_account_info(),
-            authority: self.vault_authority.to_account_info(),
-        };
-        CpiContext::new(self.token_program.to_account_info(), cpi_accounts)
-    }
-
-    fn into_close_context(&self) -> CpiContext<'_, '_, '_, 'info, CloseAccount<'info>> {
-        let cpi_accounts = CloseAccount {
-            account: self.vault.to_account_info(),
-            destination: self.initializer.to_account_info(),
-            authority: self.vault_authority.to_account_info(),
-        };
-        CpiContext::new(self.token_program.to_account_info(), cpi_accounts)
-    }
-}
-
-impl<'info> Exchange<'info> {
-    fn into_transfer_to_initializer_context(
-        &self,
-    ) -> CpiContext<'_, '_, '_, 'info, TransferChecked<'info>> {
-        let cpi_accounts = TransferChecked {
-            from: self.taker_deposit_token_account.to_account_info(),
-            mint: self.taker_deposit_token_mint.to_account_info(),
-            to: self.initializer_receive_token_account.to_account_info(),
-            authority: self.taker.to_account_info(),
-        };
-        CpiContext::new(self.token_program.to_account_info(), cpi_accounts)
-    }
-
-    fn into_transfer_to_taker_context(
-        &self,
-    ) -> CpiContext<'_, '_, '_, 'info, TransferChecked<'info>> {
-        let cpi_accounts = TransferChecked {
-            from: self.vault.to_account_info(),
-            mint: self.initializer_deposit_token_mint.to_account_info(),
-            to: self.taker_receive_token_account.to_account_info(),
-            authority: self.vault_authority.clone(),
-        };
-        CpiContext::new(self.token_program.to_account_info(), cpi_accounts)
-    }
-
-    fn into_close_context(&self) -> CpiContext<'_, '_, '_, 'info, CloseAccount<'info>> {
-        let cpi_accounts = CloseAccount {
-            account: self.vault.to_account_info(),
-            destination: self.initializer.clone(),
-            authority: self.vault_authority.clone(),
-        };
-        CpiContext::new(self.token_program.to_account_info(), cpi_accounts)
-    }
-}
-
